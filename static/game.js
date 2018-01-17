34785667960f954d2740ae3ffe8d62b06e1c4954
David Fernandez <dfernandez06@gmail.com>
2018-01-16 23:38:24 -0500

    Yeah I get promises I am def a professional

--- static/game.js
+++ static/game.js
@@ -1,11 +1,14 @@
 (function() {
-  fetch(new Request('/barkeep', {method: 'GET'}))
-    .then(response => {
-      if (response.status == 200) {
-        return (response.json());
-      }
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  const getRequest = (endpoint) => {
+    return fetch(new Request(endpoint, {method: 'GET'}))
+      .then(response => {
+        if (response.status == 200) {
+          return response.json();
+        }
+      });
+  };
+
+  getRequest('/barkeep').then((data) => {
+    console.log(data);
+  });
 })();
