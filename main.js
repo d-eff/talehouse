1e5a55030e6ddcdc4e6c4fdf59a1b33c495d8072
David Fernandez <dfernandez06@gmail.com>
2018-01-16 22:22:26 -0500

    Add support for returning an arbitrary number of items

--- main.js
+++ main.js
@@ -8,25 +8,30 @@ app.get('/', (req, res) => {
   res.json(barkeep);
 });
 
-app.get('/barkeep', (req, res, next) => {
-  res.json(getRand('barkeep'));
+app.get('/barkeep/:count?', (req, res, next) => {
+  res.json(getRand('barkeep', req.params.count));
 });
 
-app.get('/story', (req, res, next) => {
-  res.json(getRand('story'));
+app.get('/story/:count?', (req, res, next) => {
+  res.json(getRand('story', req.params.count));
 });
 
-app.get('/tellMe', (req, res, next) => {
-  res.json(getRand('tellMe'));
+app.get('/tellMe/:count?', (req, res, next) => {
+  res.json(getRand('tellMe', req.params.count));
 });
 
-app.get('/truth', (req, res, next) => {
-  res.json(getRand('truth'));
+app.get('/truth/:count?', (req, res, next) => {
+  res.json(getRand('truth', req.params.count));
 });
 
-const getRand = (section) => {
-  let sectionData = data[section];
-  return sectionData[Math.floor(Math.random() * sectionData.length)]
+const getRand = (section, count = 1) => {
+  const sectionData = data[section];
+  let response = [];
+  let max = Math.min(count, sectionData.length);
+  for(let x = 0; x < max; x++) {
+    response.push(sectionData[Math.floor(Math.random() * sectionData.length)]);
+  }
+  return response;
 }
 
 app.listen(3000, () => {
