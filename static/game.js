c0707cfc59e660ca4d46eecbc8d91c051bbce7a5
David Fernandez <dfernandez06@gmail.com>
2018-01-17 21:39:42 -0500

    Got some cards displaying. I remember how to frontend.

--- static/game.js
+++ static/game.js
@@ -1,14 +1,33 @@
-(function() {
-  const getRequest = (endpoint) => {
-    return fetch(new Request(endpoint, {method: 'GET'}))
-      .then(response => {
-        if (response.status == 200) {
-          return response.json();
-        }
-      });
-  };
+const getRequest = (endpoint) => {
+  return fetch(new Request(endpoint, {method: 'GET'}))
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      }
+    });
+};
 
-  getRequest('/barkeep').then((data) => {
-    console.log(data);
+const populateHand = (handId, numCards) => {
+  const hand = document.getElementById(`player${handId}Hand`);
+  const fragment = document.createDocumentFragment();
+
+  getRequest(`/barkeep/${numCards}`).then((data) => {
+    for (var counter = 0; counter < numCards; counter++) {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.textContent = data[counter];
+      fragment.appendChild(card);
+    }
+    hand.appendChild(fragment);
   });
-})();
+};
+
+const main = () => {
+  populateHand('1', '5');
+  populateHand('2', '5');
+  populateHand('3', '5');
+  populateHand('4', '5');
+};
+
+main();
+  
