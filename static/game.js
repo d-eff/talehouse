e43baae63a60a8dde6f51f35a7a6746bcde514d5
David Fernandez <dfernandez06@gmail.com>
2018-01-17 21:45:32 -0500

    DIG THROUGH THE DITCHES

--- static/game.js
+++ static/game.js
@@ -7,6 +7,8 @@ const getRequest = (endpoint) => {
     });
 };
 
+const containers = dragula();
+
 const populateHand = (handId, numCards) => {
   const hand = document.getElementById(`player${handId}Hand`);
   const fragment = document.createDocumentFragment();
@@ -19,14 +21,18 @@ const populateHand = (handId, numCards) => {
       fragment.appendChild(card);
     }
     hand.appendChild(fragment);
+    containers.containers.push(hand);
   });
 };
 
 const main = () => {
+  const story = document.getElementById('story');
+  containers.containers.push(story);
   populateHand('1', '5');
   populateHand('2', '5');
   populateHand('3', '5');
   populateHand('4', '5');
+  
 };
 
 main();
